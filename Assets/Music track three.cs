5daa8bf22c2b306bf6cc818b7619d1b71da7bbbd
RaoulEneskjoldLBS <raoul.eneskjold@elev.ga.lbs.se>
2024-04-17 08:18:31 +0200

    s

---
+++ Assets/Music track three.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Musictrackthree : MonoBehaviour
+{
+
+    MeshRenderer mr;
+    AudioSource music;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+        mr.material.color = Color.green;
+        music = GetComponent<AudioSource>();
+        music.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            mr.material.color = Color.red;
+            music.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Space))
+        {
+            mr.material.color = Color.green;
+            music.Stop();
+        }
+    }
+}
